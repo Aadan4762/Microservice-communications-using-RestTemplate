799cf89d38b1a0b6d8f28d5f72f33ec584b57192
adesh <aadan4762@gmail.com>
2024-02-14 08:32:58 +0300

    config server implementation

--- employee-service/src/main/java/com/example/Employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/example/Employeeservice/controller/EmployeeController.java
@@ -9,9 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
 
 import java.util.List;
 import java.util.Optional;
